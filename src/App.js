4d1d995f76722cb37eb805c26c7d98f6ed0e1c09
Ben Rehmann <brehmann@me.com>
2024-05-28 14:46:33 -0500

    creating tictactoe

--- src/App.js
+++ src/App.js
@@ -2,22 +2,33 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  // what do I need
+  // players / 2
+  // click on divs
+  // 
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="gameTicTacToe">
+      <div className='gameBoard'>
+        <div className='topSquares'>
+          <div className='square topLeft'></div>
+          <div className='square topMiddle'></div>
+          <div className='square topRight'></div>
+        </div>
+        <div className='middleSquares'>
+          <div className='square middleLeft'></div>
+          <div className='square middleMiddle'></div>
+          <div className='square middleRight'></div>
+        </div>
+        <div className='bottomSquares'>
+          <div className='square bottomLeft'></div>
+          <div className='square bottomMiddle'></div>
+          <div className='square bottomRight'></div>
+        </div>
+      </div>
     </div>
   );
 }
