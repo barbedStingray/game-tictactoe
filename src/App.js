0afcab143b334d39578a42aab45b4a7d151b11b9
Ben Rehmann <brehmann@me.com>
2024-05-28 15:48:42 -0500

    tictac game complete

--- src/App.js
+++ src/App.js
@@ -1,32 +1,96 @@
 import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
 
-  // what do I need
-  // players / 2
-  // click on divs
-  // 
 
+  let [playerSymbol, setPlayerSymbol] = useState('X');
+
+  let [gameSpaces, setGameSpaces] = useState({
+    topLeft: null,
+    topMiddle: null,
+    topRight: null,
+    middleLeft: null,
+    middleMiddle: null,
+    middleRight: null,
+    bottomLeft: null,
+    bottomMiddle: null,
+    bottomRight: null
+  });
+
+  const winningCombinations = [
+    ['topLeft', 'topMiddle', 'topRight'],
+    ['middleLeft', 'middleMiddle', 'middleRight'],
+    ['bottomLeft', 'bottomMiddle', 'bottomRight'],
+    ['topLeft', 'middleLeft', 'bottomLeft'],
+    ['topMiddle', 'middleMiddle', 'bottomMiddle'],
+    ['topRight', 'middleRight', 'bottomRight'],
+    ['topLeft', 'middleMiddle', 'bottomRight'],
+    ['topRight', 'middleMiddle', 'bottomLeft']
+  ];
+
+  function checkForWinner(spaces) {
+    console.log('spaces', spaces);
+    for (let combination of winningCombinations) {
+      // console.log('combination', combination);
+      const [a, b, c] = combination;
+      // console.log('a', a);
+      // console.log('a, b, c', a, b, c);
+      // console.log('space a, b, c', spaces[a], spaces[b], spaces[c]);
+      if (spaces[a] && spaces[a] === spaces[b] && spaces[a] === spaces[c]) {
+        return spaces[a];
+      }
+    }
+    return null;
+  }
+
+
+  function takeATurn(square) {
+    console.log('taking a turn');
+    const newGameSpaces = { ...gameSpaces };
+
+    // valid space to play check
+    if (newGameSpaces[square] !== null) {
+      console.log('this space is taken!');
+      alert('This square is taken!');
+      return;
+    } else {
+
+      newGameSpaces[square] = playerSymbol;
+      console.log('square affected', gameSpaces[square]);
+      setGameSpaces(newGameSpaces);
+
+      const winner = checkForWinner(newGameSpaces);
+      if (winner !== null) {
+        // alert(`The ${winner}'s have it!`);
+        return;
+      }
+    }
+    setPlayerSymbol(playerSymbol === 'X' ? 'O' : 'X');
+  }
 
 
   return (
     <div className="gameTicTacToe">
+      <div className='turnDisplay'>
+        <p>Player 1's turn</p>
+      </div>
       <div className='gameBoard'>
         <div className='topSquares'>
-          <div className='square topLeft'></div>
-          <div className='square topMiddle'></div>
-          <div className='square topRight'></div>
+          <div onClick={() => takeATurn('topLeft')} className='square topLeft'>{gameSpaces.topLeft}</div>
+          <div onClick={() => takeATurn('topMiddle')} className='square topMiddle'>{gameSpaces.topMiddle}</div>
+          <div onClick={() => takeATurn('topRight')} className='square topRight'>{gameSpaces.topRight}</div>
         </div>
         <div className='middleSquares'>
-          <div className='square middleLeft'></div>
-          <div className='square middleMiddle'></div>
-          <div className='square middleRight'></div>
+          <div onClick={() => takeATurn('middleLeft')} className='square middleLeft'>{gameSpaces.middleLeft}</div>
+          <div onClick={() => takeATurn('middleMiddle')} className='square middleMiddle'>{gameSpaces.middleMiddle}</div>
+          <div onClick={() => takeATurn('middleRight')} className='square middleRight'>{gameSpaces.middleRight}</div>
         </div>
         <div className='bottomSquares'>
-          <div className='square bottomLeft'></div>
-          <div className='square bottomMiddle'></div>
-          <div className='square bottomRight'></div>
+          <div onClick={() => takeATurn('bottomLeft')} className='square bottomLeft'>{gameSpaces.bottomLeft}</div>
+          <div onClick={() => takeATurn('bottomMiddle')} className='square bottomMiddle'>{gameSpaces.bottomMiddle}</div>
+          <div onClick={() => takeATurn('bottomRight')} className='square bottomRight'>{gameSpaces.bottomRight}</div>
         </div>
       </div>
     </div>
